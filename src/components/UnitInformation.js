b6fd2d8a8e43e8129ca767a275529dff694c3b5b
TanuSharma0802 <tanuvashishtha802@gmail.com>
2024-06-28 16:56:12 +0530

    UI Changes

--- src/components/UnitInformation.js
+++ src/components/UnitInformation.js
@@ -45,123 +45,261 @@ const UnitInformation = ({ nextStep }) => {
 
 
     return (
-        <div className="flex flex-1 justify-between px-4 pt-2 bg-gray-100 rounded-lg shadow-lg w-3/4 m-auto flex-wrap">
-            <div className="w-1/2 p-2 flex flex-col">
-                <h2 className="text-xl font-bold text-blue-600 mb-2">Unit Information</h2>
-                <p className="mb-4 text-gray-700">Enter the width, length, height, and weight of each unit below.</p>
+       
+    //     <div className="flex flex-1 justify-between px-8 pt-6 pb-8 bg-white rounded-lg shadow-md w-3/4 m-auto flex-wrap">
+    //     <div className="w-full lg:w-1/2 p-4 flex flex-col">
+    //       <h2 className="text-2xl font-bold text-blue-700 mb-4">Unit Information</h2>
+    //       <p className="mb-6 text-gray-800">Enter the width, length, height, and weight of each unit below.</p>
+  
+    //       <form onSubmit={handleSubmit} className="flex flex-col space-y-6">
+    //         <div>
+    //           <label className="block text-lg font-semibold mb-2 text-gray-900">Unit Dimensions (in Cm)</label>
+    //           <div className="flex space-x-4">
+    //             <div className="flex flex-col">
+    //               <label className="text-sm mb-2 text-black-800">Width</label>
+    //               <input
+    //                 type="number"
+    //                 name="width"
+    //                 value={unitData.width}
+    //                 onChange={handleChange}
+    //                 required
+    //                 className="w-24 px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-600"
+    //               />
+    //             </div>
+    //             <div className="flex flex-col">
+    //               <label className="text-sm mb-2 text-black-800">Length</label>
+    //               <input
+    //                 type="number"
+    //                 name="length"
+    //                 value={unitData.length}
+    //                 onChange={handleChange}
+    //                 required
+    //                 className="w-24 px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-600"
+    //               />
+    //             </div>
+    //             <div className="flex flex-col">
+    //               <label className="text-sm mb-2 text-black-800">Height</label>
+    //               <input
+    //                 type="number"
+    //                 name="height"
+    //                 value={unitData.height}
+    //                 onChange={handleChange}
+    //                 required
+    //                 className="w-24 px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-600"
+    //               />
+    //             </div>
+    //           </div>
+    //         </div>
+  
+    //         <div>
+    //           <label className="block text-lg font-semibold mb-2 text-gray-900">Unit Weight (in Kg)</label>
+    //           <input
+    //             type="number"
+    //             name="weight"
+    //             value={unitData.weight}
+    //             onChange={handleChange}
+    //             required
+    //             className="w-32 px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-600"
+    //           />
+    //         </div>
+  
+    //         <div>
+    //           <label className="block text-lg font-semibold mb-2 text-gray-900">Pallet Dimensions (in Cm)</label>
+    //           <div className="flex space-x-4">
+    //             <div className="flex flex-col">
+    //               <label className="text-sm mb-2 text-black-700">Pallet Width</label>
+    //               <input
+    //                 type="number"
+    //                 name="palletWidth"
+    //                 value={unitData.palletWidth}
+    //                 onChange={handleChange}
+    //                 required
+    //                 className="w-32 px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-600"
+    //               />
+    //             </div>
+    //             <div className="flex flex-col">
+    //               <label className="text-sm mb-2 text-black-700">Pallet Length</label>
+    //               <input
+    //                 type="number"
+    //                 name="palletLength"
+    //                 value={unitData.palletLength}
+    //                 onChange={handleChange}
+    //                 required
+    //                 className="w-32 px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-600"
+    //               />
+    //             </div>
+    //           </div>
+    //         </div>
+  
+    //         <div>
+    //           <label className="block text-lg font-semibold mb-2 text-gray-900">Clearance</label>
+    //           <div className="flex items-center space-x-4">
+    //             <input
+    //               type="checkbox"
+    //               name="clearanceEnabled"
+    //               checked={unitData.clearanceEnabled}
+    //               onChange={handleChange}
+    //               className="h-5 w-5 text-blue-600 focus:ring-blue-600 border-gray-300 rounded"
+    //             />
+    //             <input
+    //               type="number"
+    //               name="clearance"
+    //               value={unitData.clearance}
+    //               onChange={handleChange}
+    //               disabled={!unitData.clearanceEnabled}
+    //               className="w-32 px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-600 disabled:bg-gray-200"
+    //             />
+    //           </div>
+    //         </div>
+  
+    //         <div className="flex justify-between mt-6">
+    //           <button
+    //             type="button"
+    //             className="px-4 py-2 bg-gray-300 text-gray-900 rounded hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-600"
+    //           >
+    //             Previous Step
+    //           </button>
+    //           <button
+    //             type="submit"
+    //             className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600"
+    //           >
+    //             Next Step
+    //           </button>
+    //         </div>
+    //       </form>
+    //     </div>
+  
+    //     <div className="w-full lg:w-1/2 flex items-center justify-center p-4">
+    //       <img src={`${process.env.PUBLIC_URL}/box.png`} alt="box" className="max-w-full h-auto rounded shadow-lg" />
+    //     </div>
+    //   </div>
+    <div className="flex flex-1 justify-between px-4 pt-4 pb-4 bg-white rounded-lg shadow-md w-4/5 m-auto flex-wrap">
+      <div className="w-full lg:w-1/2 p-2 flex flex-col">
+        <h2 className="text-lg font-bold text-blue-700 mb-2">Unit Information</h2>
+        <p className="mb-4 text-gray-800">Enter the width, length, height, and weight of each unit below.</p>
 
-                <form onSubmit={handleSubmit} className="flex flex-col flex-wrap space-y-4">
-                    <div>
-                        <label className="block text-lg font-semibold mb-1 text-gray-800">Unit Dimensions (in Cm)</label>
-                        <div className="flex space-x-2">
-                            <div className="flex flex-col">
-                                <label className="text-sm mb-1 text-gray-600">Width</label>
-                                <input
-                                    type="number"
-                                    name="width"
-                                    value={unitData.width}
-                                    onChange={handleChange}
-                                    required
-                                    className="w-16 px-2 py-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                />
-                            </div>
-                            <div className="flex flex-col">
-                                <label className="text-sm mb-1 text-gray-600">Length</label>
-                                <input
-                                    type="number"
-                                    name="length"
-                                    value={unitData.length}
-                                    onChange={handleChange}
-                                    required
-                                    className="w-16 px-2 py-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                />
-                            </div>
-                            <div className="flex flex-col">
-                                <label className="text-sm mb-1 text-gray-600">Height</label>
-                                <input
-                                    type="number"
-                                    name="height"
-                                    value={unitData.height}
-                                    onChange={handleChange}
-                                    required
-                                    className="w-16 px-2 py-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                />
-                            </div>
-                        </div>
-                    </div>
-
-                    <div>
-                        <label className="block text-lg font-semibold mb-1 text-gray-800">Unit Weight (in Kg)</label>
-                        <input
-                            type="number"
-                            name="weight"
-                            value={unitData.weight}
-                            onChange={handleChange}
-                            required
-                            className="w-20 px-2 py-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        />
-                    </div>
-
-                    <div>
-                        <label className="block text-lg font-semibold mb-1 text-gray-800">Pallet Dimensions (in Cm)</label>
-                        <div className="flex space-x-2">
-                            <div className="flex flex-col">
-                                <label className="text-sm mb-1 text-gray-600">Pallet Width</label>
-                                <input
-                                    type="number"
-                                    name="palletHeight"
-                                    value={unitData.palletHeight}
-                                    onChange={handleChange}
-                                    required
-                                    className="w-20 px-2 py-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                />
-                            </div>
-                            <div className="flex flex-col">
-                                <label className="text-sm mb-1 text-gray-600">Pallet Length</label>
-                                <input
-                                    type="number"
-                                    name="palletWidth"
-                                    value={unitData.palletWidth}
-                                    onChange={handleChange}
-                                    required
-                                    className="w-20 px-2 py-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                />
-                            </div>
-                        </div>
-                    </div>
-
-                    <div>
-                        <label className="block text-lg font-semibold mb-1 text-gray-800">Clearance</label>
-                        <div className="flex items-center space-x-2">
-                            <input
-                                type="checkbox"
-                                name="clearanceEnabled"
-                                checked={unitData.clearanceEnabled}
-                                onChange={handleChange}
-                                className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
-                            />
-                            <input
-                                type="number"
-                                name="clearance"
-                                value={unitData.clearance}
-                                onChange={handleChange}
-                                disabled={!unitData.clearanceEnabled}
-                                className="w-20 px-2 py-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:bg-gray-200"
-                            />
-                        </div>
-                    </div>
-
-                    <div className="flex justify-between">
-                        <button type="button" className="px-3 py-1 bg-gray-300 text-gray-800 rounded hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500">Previous Step</button>
-                        <button type="submit" className="px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">Next Step</button>
-                    </div>
-                </form>
+        <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
+          <div>
+            <label className="block text-lg font-semibold mb-1 text-gray-900">Unit Dimensions (in Cm)</label>
+            <div className="flex space-x-2">
+              <div className="flex flex-col">
+                <label className="text-sm mb-1 text-gray-700">Width</label>
+                <input
+                  type="number"
+                  name="width"
+                  value={unitData.width}
+                  onChange={handleChange}
+                  required
+                  className="w-20 px-2 py-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-600"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label className="text-sm mb-1 text-gray-700">Length</label>
+                <input
+                  type="number"
+                  name="length"
+                  value={unitData.length}
+                  onChange={handleChange}
+                  required
+                  className="w-20 px-2 py-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-600"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label className="text-sm mb-1 text-gray-700">Height</label>
+                <input
+                  type="number"
+                  name="height"
+                  value={unitData.height}
+                  onChange={handleChange}
+                  required
+                  className="w-20 px-2 py-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-600"
+                />
+              </div>
             </div>
+          </div>
 
-            <div className="w-1/2 flex items-center justify-center p-2">
-                <img src={`${process.env.PUBLIC_URL}/box.png`} alt="box" className="max-w-full h-auto rounded shadow-lg" />
+          <div>
+            <label className="block text-lg font-semibold mb-1 text-gray-900">Unit Weight (in Kg)</label>
+            <input
+              type="number"
+              name="weight"
+              value={unitData.weight}
+              onChange={handleChange}
+              required
+              className="w-24 px-2 py-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-600"
+            />
+          </div>
+
+          <div>
+            <label className="block text-lg font-semibold mb-1 text-gray-900">Pallet Dimensions (in Cm)</label>
+            <div className="flex space-x-2">
+              <div className="flex flex-col">
+                <label className="text-sm mb-1 text-gray-700">Pallet Width</label>
+                <input
+                  type="number"
+                  name="palletWidth"
+                  value={unitData.palletWidth}
+                  onChange={handleChange}
+                  required
+                  className="w-24 px-2 py-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-600"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label className="text-sm mb-1 text-gray-700">Pallet Length</label>
+                <input
+                  type="number"
+                  name="palletLength"
+                  value={unitData.palletLength}
+                  onChange={handleChange}
+                  required
+                  className="w-24 px-2 py-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-600"
+                />
+              </div>
             </div>
-        </div>
+          </div>
+
+          <div>
+            <label className="block text-lg font-semibold mb-1 text-gray-900">Clearance</label>
+            <div className="flex items-center space-x-2">
+              <input
+                type="checkbox"
+                name="clearanceEnabled"
+                checked={unitData.clearanceEnabled}
+                onChange={handleChange}
+                className="h-4 w-4 text-blue-600 focus:ring-blue-600 border-gray-300 rounded"
+              />
+              <input
+                type="number"
+                name="clearance"
+                value={unitData.clearance}
+                onChange={handleChange}
+                disabled={!unitData.clearanceEnabled}
+                className="w-24 px-2 py-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-600 disabled:bg-gray-200"
+              />
+            </div>
+          </div>
+
+          <div className="flex justify-between mt-4">
+            <button
+              type="button"
+              className="px-3 py-1 bg-gray-300 text-gray-900 rounded hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-600"
+            >
+              Previous Step
+            </button>
+            <button
+              type="submit"
+              className="px-3 py-1 bg-blue-600 text-white rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600"
+            >
+              Next Step
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <div className="w-full lg:w-1/2 flex items-center justify-center p-2">
+        <img src={`${process.env.PUBLIC_URL}/box.png`} alt="box" className="max-w-full h-auto rounded shadow-lg" />
+      </div>
+    </div>
     );
 };
 
